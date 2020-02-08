bf891f22092fa9637ee0fcf12c4b5e7178b51af8
sobiemir <sobiemir@aculo.pl>
2020-02-08 10:27:01 +0100

    Change response function definition in interface

--- src/Interfaces/ActionDescriptorInterface.php
+++ src/Interfaces/ActionDescriptorInterface.php
@@ -4,14 +4,13 @@ namespace Waxwing\Interfaces;
 
 interface ActionDescriptorInterface
 {
-    function setRenderers(array $renderers): void;
-    function getRenderers(): array;
     function setUriParameters(array $parameters): void;
     function getUriParameters(): array;
     function setBodyDescriptor(string $class, bool $required = true, array $availableParsers = null): void;
     function getBodyParsers(): array;
     function getBodyClass(): string;
-    function setResponseModel(string $model): void;
-    function getResponseModel(): string;
     function isBodyRequired(): bool;
+    function setResponseDescriptor(string $class, array $availableRenderers = null): void;
+    function getResponseRenderers(): array;
+    function getResponseClass(): string;
 }
