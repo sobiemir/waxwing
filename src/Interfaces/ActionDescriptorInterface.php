1ffb62ad3fe151abfacabca3164a87c4c5179eac
sobiemir <sobiemir@aculo.pl>
2020-02-08 11:57:21 +0100

    Add nullable class name in body and response descriptors

--- src/Interfaces/ActionDescriptorInterface.php
+++ src/Interfaces/ActionDescriptorInterface.php
@@ -6,11 +6,11 @@ interface ActionDescriptorInterface
 {
     function setUriParameters(array $parameters): void;
     function getUriParameters(): array;
-    function setBodyDescriptor(string $class, bool $required = true, array $availableParsers = null): void;
+    function setBodyDescriptor(?string $class, bool $required = true, array $availableParsers = null): void;
     function getBodyParsers(): array;
     function getBodyClass(): string;
     function isBodyRequired(): bool;
-    function setResponseDescriptor(string $class, int $responseCode = 200, array $availableRenderers = null): void;
+    function setResponseDescriptor(?string $class, int $responseCode = 200, array $availableRenderers = null): void;
     function getResponseRenderers(): array;
     function getResponseClass(): string;
     function getResponseCode(): int;
